4259b34cf07d14c01b3f513b08eecf719b7cb18d
Robert Hammond <hamrobe502@gmail.com>
2022-01-31 19:59:10 -0500

    comments and logs for basic user code

--- api/src/main/java/infidelity/api/controller/UserController.java
+++ api/src/main/java/infidelity/api/controller/UserController.java
@@ -4,6 +4,8 @@ import infidelity.api.data.User;
 import infidelity.api.data.model.UserData;
 import infidelity.api.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +15,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/users")
+@RequestMapping(path = "/user")
 @Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    /**
+     * Default endpoint for retrieving user information given their user id.
+     * @param id The ID created by AWS Cognito which serves as a mandatory unique identifier for each user
+     * @return JSON-friendly UserData object containing basic information about the user.
+     */
     @GetMapping("/{id}")
-    public UserData getUser(@PathVariable("id") String id) {
+    public ResponseEntity<UserData> getUser(@PathVariable("id") String id) {
+        log.info("GET /user/{}", id);
         Optional<User> user = userService.findUserById(id);
-        return user.get().toData();
+        if (user.isPresent()) {
+            return new ResponseEntity<>(user.get().toData(), HttpStatus.OK);
+        } else {
+            log.warn("User {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- api/src/main/java/infidelity/api/data/model/UserData.java
+++ api/src/main/java/infidelity/api/data/model/UserData.java
@@ -2,6 +2,10 @@ package infidelity.api.data.model;
 
 import lombok.*;
 
+/**
+ * JSON-friendly data object that can be thrown to the frontend representing a single user.
+ * Can only contain valid JSON datatypes: Integers, Strings, Booleans, arrays, objects (JSON Object).
+ */
 @Data
 @Builder
 @NoArgsConstructor
