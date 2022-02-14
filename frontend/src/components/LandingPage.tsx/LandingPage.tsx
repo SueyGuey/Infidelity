15dddec21ad1f35c4800f084641789537be93eb7
Suhan Gui <suhangui@Suhans-MacBook-Pro-3.local>
2022-02-13 20:28:57 -0500

    Started Landing Page

---
+++ frontend/src/components/LandingPage.tsx/LandingPage.tsx
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+
+class LandingPage extends Component{
+    render(){
+        return(
+            <div>
+                <p>Infidelity</p>
+            </div>
+        );
+    }
+}
+
+var styling = {
+    
+}
+
+export default LandingPage
