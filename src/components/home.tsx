cdfc3b6d9d0f7fedc15fd7789b4c3bed4b5bdf55
Suhan Gui <suhangui@Suhans-MacBook-Pro-3.local>
2022-02-19 20:34:19 -0500

    added sign up box

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import Login from '../components/login';
+import Signup from './Signup';
 import TopNavBar from './TopNavBar';
 
 export default function Home(): ReactElement {
@@ -8,6 +9,7 @@ export default function Home(): ReactElement {
 			<TopNavBar/>
 			<p>Infidelity</p>
 			<Login />
+			<Signup/>
 		</div>
 	);
 }
