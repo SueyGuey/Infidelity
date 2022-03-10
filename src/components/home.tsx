cf00d10dd2228c0c960a704877dccdd82ec4e13a
Robert Hammond <hamrobe502@gmail.com>
2022-03-09 19:18:27 -0600

    login styling

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,40 +1,15 @@
-import React, { ReactElement, useState } from 'react';
-import Login from './login';
-import Signup from './signup';
+import React, { ReactElement } from 'react';
 import PitchRectangles from './PitchRectangles';
 import TopNavBar from './TopNavBar';
 import '../css/home.css';
+import LoginSignup from './LoginSignup';
 
 export default function Home(): ReactElement {
-	const [tabNumber, setTabNumber] = useState(1);
-
 	return (
 		<div>
 			<TopNavBar/>
-			<div className='container'>
-				<div className="tabContainer">
-					<button
-					className={tabNumber === 1 ? "tabs active-tabs" : "tabs"}
-					onClick={() => setTabNumber(1)}>
-						Log In
-					</button>
-					<button
-					className={tabNumber === 2 ? "tabs active-tabs" : "tabs"}
-					onClick={() => setTabNumber(2)}>
-						Sign Up
-					</button>
-				</div>
-				
-				<div className="contentContainer">
-					<div className={tabNumber === 1 ? "content  active-content" : "content"}>
-						<Login />
-					</div>
-					<div className={tabNumber === 2 ? "content  active-content" : "content"}>
-						<Signup/>
-					</div>
-				</div>
-			</div>
-      <PitchRectangles/>
+			<LoginSignup/>
+			<PitchRectangles/>
 		</div>
 	);
 }
