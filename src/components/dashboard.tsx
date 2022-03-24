89eeb2b391036a6732949715d8a0398ba610ca9a
kySanDSML <71370777+kySanDSML@users.noreply.github.com>
2022-03-24 04:35:32 -0400

    Dashboard Side-Menu

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,15 +1,18 @@
 import React, { ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 import userPool from '../authentication/userPool';
+import DashSideMenu from './DashSideMenu';
 import TopNavBar from './TopNavBar';
+import '../css/Dashboard.css'
 
 export default function Dashboard(): ReactElement {
 	const user = userPool.getCurrentUser();
-	return user ? (
-		<div>
-			<TopNavBar/>
-			<p>Welcome to the Dashboard :)</p>
-			<p>Username: {user.getUsername()}</p>
+	return /*user ?*/ (
+		<div className = "Dashboard">
+			{/* <TopNavBar/> */}
+			<DashSideMenu/>
+			<div className = "mainDash">
+			</div>
 		</div>
-	) : <Navigate to="/"/>;
+	) /* : <Navigate to="/"/>*/;
 }
