ac5069d051789be32da9f3b9fc30ebae15881730
Robert Hammond <hamrobe502@gmail.com>
2022-03-28 18:12:19 -0400

    MarketService outline

--- api/src/main/java/infidelity/api/data/repository/StockRepository.java
+++ api/src/main/java/infidelity/api/data/repository/StockRepository.java
@@ -1,7 +1,8 @@
 package infidelity.api.data.repository;
 
 import infidelity.api.data.Stock;
+import infidelity.api.data.Tradeable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockRepository extends JpaRepository<Stock, String> {
+public interface StockRepository extends JpaRepository<Tradeable, String> {
 }
--- api/src/main/java/infidelity/api/service/MarketService.java
+++ api/src/main/java/infidelity/api/service/MarketService.java
@@ -1,10 +1,15 @@
 package infidelity.api.service;
 
+import infidelity.api.data.ChangingNumber;
+import infidelity.api.data.Tradeable;
 import infidelity.api.data.repository.StockRepository;
 import infidelity.api.stockdata.FinnHub;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -33,10 +38,27 @@ public class MarketService {
         return fh.getInfo(symbol).getPrice();
     }
 
-    /**
-     *
-     */
-    public void populateCompanyInfo() {
+    public void updateMarket() {
+        // update general market information, the function only to be called
+        // once per day or triggered manually
+        // populate company information
+    }
+
+    public Tradeable getInfo(String symbol) {
+        return stockRepository.getById(symbol);
+    }
+
+    public ChangingNumber getPrice(Tradeable item) {
+        // TODO: update the price
+        return item.getCurrentPrice();
+    }
+
+    public List<Tradeable> searchMarket(String query) {
+        return new ArrayList<>();
+    }
+
+    private List<String> listSymbols() {
         fh.listExchange();
+        return new ArrayList<>();
     }
 }
