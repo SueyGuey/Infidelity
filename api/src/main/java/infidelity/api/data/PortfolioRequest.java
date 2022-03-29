fa16695158076f7b260e509edca448c81e7d44cd
Suhan Gui <suhangui@Suhans-MacBook-Pro-3.local>
2022-03-29 19:01:37 -0400

    removed unnecessary imports

--- api/src/main/java/infidelity/api/data/PortfolioRequest.java
+++ api/src/main/java/infidelity/api/data/PortfolioRequest.java
@@ -1,10 +1,5 @@
 package infidelity.api.data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.util.UUID;
-
 public class PortfolioRequest {
     private String username;
     private String portfolioName;
