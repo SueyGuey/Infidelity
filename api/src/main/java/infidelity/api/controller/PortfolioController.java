6a7856d71b20093b445fad6a534c9045ebb5d7a0
Suhan Gui <suhangui@Suhans-MacBook-Pro-3.local>
2022-03-29 17:46:47 -0400

    added functions for PortfolioController

--- api/src/main/java/infidelity/api/controller/PortfolioController.java
+++ api/src/main/java/infidelity/api/controller/PortfolioController.java
@@ -1,8 +1,12 @@
 package infidelity.api.controller;
 
+import infidelity.api.data.Portfolio;
+import infidelity.api.data.PortfolioRequest;
+import infidelity.api.data.TransactionRequest;
 import infidelity.api.service.PortfolioService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class PortfolioController {
     @Autowired
     PortfolioService portfolioService;
+
+    //TODO: implement function
+    public ResponseEntity<Portfolio> createPortfolio(PortfolioRequest request){
+        return null;
+    }
+
+    //TODO: implement function
+    public ResponseEntity deletePortfolio(String username, String portfolioName){
+        return null;
+    }
+
+    //TODO: implement function
+    public ResponseEntity<TransactionRequest> makeTransaction(TransactionRequest request){
+        return null;
+    }
+
+    //TODO: implement function
+    public void updateHistory(String username, String portfolioName){
+        return;
+    }
 }
